67427a92fb0a5bf37c82efd2cf26b7465e034669
poyboodong <poy.alev@boodong.kr>
2023-03-06 13:23:51 +0900

    Add: line chart with react-native-svg-chart

--- src/navigators/HomeStack.jsx
+++ src/navigators/HomeStack.jsx
@@ -1,11 +1,16 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ChartScreen from '../screens/ChartScreen';
 import HomeScreen from '../screens/HomeScreen';
 const HomeStack = () => {
   const Stack = createNativeStackNavigator();
   return (
     <Stack.Navigator>
       <Stack.Screen //
-        name="Stack"
+        name="Chart"
+        component={ChartScreen}
+      />
+      <Stack.Screen //
+        name="Event"
         component={HomeScreen}
       />
     </Stack.Navigator>
